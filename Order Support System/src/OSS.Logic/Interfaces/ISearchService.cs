9dd6d9c156c74b8658012599b311f432d05937e6
AhimasASP <potetenko.alexey@gmail.com>
2020-04-30 20:04:19 +0300

    SerrchService added

---
+++ Order Support System/src/OSS.Logic/Interfaces/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OSS.Domain.Common.Models.ApiModels;
+
+namespace OSS.Domain.Interfaces.Services
+{
+    public interface ISearchService
+    {
+        Task<List<SearchResponseModel>> Search(string value);
+    }
+}
